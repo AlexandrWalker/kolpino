fe755eece047b1d70f043deabd72d5528fab3ffd
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-01-19 22:03:34 +0700

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    (function () {
+      var filterBtn = document.querySelectorAll('.filter__btn'),
+        active = document.getElementsByClassName('active');
+
+      Array.from(filterBtn).forEach(function (item, i, filterBtn) {
+        item.addEventListener('click', function (e) {
+          if (active.length > 0 && active[0] !== this)
+            active[0].classList.remove('active');
+
+          this.classList.toggle('active');
+        });
+      });
+    })();
+
+    (function () {
+      var pagBtn = document.querySelectorAll('.pagination__link'),
+        pagActive = document.getElementsByClassName('pagination__active');
+
+      Array.from(pagBtn).forEach(function (item, i, pagBtn) {
+        item.addEventListener('click', function (e) {
+          if (pagActive.length > 0 && pagActive[0] !== this)
+            pagActive[0].classList.remove('pagination__active');
+
+          this.classList.toggle('pagination__active');
+        });
+      });
+    })();
+
+  });
+})();
